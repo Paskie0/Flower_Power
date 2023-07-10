6d5bcd9211797be9effb97670f8dabb7864dc7ff
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-10 04:54:38 +0200

    destroy boom

--- logout.php
+++ logout.php
@@ -1,7 +1,5 @@
 <?php
 session_start(); // Start the session
-unset($_SESSION['loggedin']);
-unset($_SESSION['user_id']);
-unset($_SESSION['username']);
+session_destroy();
 header("Location: index.php");
 exit();
