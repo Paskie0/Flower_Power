82c6393396e4d5864c02905c8067138d4837e5b0
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-10 05:17:09 +0200

    pls be good

--- medewerker-info.php
+++ medewerker-info.php
@@ -4,19 +4,19 @@ include_once 'connect.php';
 $medewerker = $_SESSION['medewerker_id']; // Assuming you have the klant_id stored in a session variable
 
 // Fetch the user data from the database based on klant_id
-$sql = "SELECT * FROM medewerkers WHERE klant_id = '$klant_id'";
+$sql = "SELECT * FROM medewerkers WHERE medewerker_id = '$medewerker'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
 
     // Extract the values from the retrieved row
-    $email = $row['klant_email'];
-    $password = $row['klant_wachtwoord'];
-    $firstName = $row['klant_voornaam'];
-    $infix = $row['klant_tussenvoegsel'];
-    $lastName = $row['klant_achternaam'];
-    $dateOfBirth = $row['klant_geboortedatum'];
+    $email = $row['medewerker_email'];
+    $password = $row['medewerker_wachtwoord'];
+    $firstName = $row['medewerker_voornaam'];
+    $infix = $row['medewerker_tussenvoegsel'];
+    $lastName = $row['medewerker_achternaam'];
+    $dateOfBirth = $row['medewerker_geboortedatum'];
 }
 
 $conn->close();
