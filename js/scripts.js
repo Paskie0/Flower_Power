4ca46dc43c753e19ad7d9436da88c63992850b3b
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 01:19:39 +0200

    fixed the previous commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,19 @@
 const themeSwitcher = document.querySelector("#theme-switcher");
-const currentTheme = localStorage.getItem("theme") || "halloween";
-document.documentElement.setAttribute("data-theme", currentTheme);
-themeSwitcher.checked = currentTheme === "retro";
-themeSwitcher.addEventListener("change", function () {
-  const newTheme = themeSwitcher.checked ? "retro" : "halloween";
-  document.documentElement.setAttribute("data-theme", newTheme);
-  localStorage.setItem("theme", newTheme);
+themeSwitcher.addEventListener("change", () => {
+  document.documentElement.setAttribute("data-theme", themeSwitcher.checked ? "retro" : "halloween");
+  localStorage.setItem("data-theme", themeSwitcher.checked ? "retro" : "halloween");
 });
 
 function toggleForms() {
-  const login = document.querySelector("#login");
-  const register = document.querySelector("#register");
-  const toggleFormsButton = document.querySelector("#toggleFormsButton");
-  login.classList.toggle("hidden");
-  register.classList.toggle("hidden");
-  if (login.classList.contains("hidden")) {
-    toggleFormsButton.innerHTML = "Login";
-  } else {
-    toggleFormsButton.innerHTML = "Registreren";
-  }
+  document.querySelector("#login").classList.toggle("hidden");
+  document.querySelector("#register").classList.toggle("hidden");
+  if (document.querySelector("#login").classList.contains("hidden"))
+    document.querySelector("#toggleFormsButton").innerHTML = "Login";
+  else document.querySelector("#toggleFormsButton").innerHTML = "Registreren";
 }
 
 function medewerkerForm() {
-  const medewerkerLogin = document.querySelector("#medewerker-login");
-  const login = document.querySelector("#login");
-  const medewerkerFormButton = document.querySelector("#medewerkerFormButton");
-  medewerkerLogin.classList.toggle("hidden");
-  login.classList.toggle("hidden");
-  medewerkerFormButton.classList.toggle("btn-primary");
+  document.querySelector("#medewerker-login").classList.toggle("hidden");
+  document.querySelector("#login").classList.toggle("hidden");
+  document.querySelector("#medewerkerFormButton").classList.toggle("btn-primary");
 }
