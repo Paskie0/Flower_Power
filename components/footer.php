4e1accb447a1ff21c9b4d30c80b85b18ae55088d
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-11 20:33:09 +0200

    header works, now footer

---
+++ components/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer items-center p-4 bg-neutral text-neutral-content">
+    <div class="items-center grid-flow-col">
+        <p>@ 2023 Flower Power - All right reserved</p>
+    </div>
+    <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <a><i class="fa-brands fa-tiktok fa-xl"></i></a>
+        <a><i class="fa-brands fa-youtube fa-xl"></i></a>
+        <a><i class="fa-brands fa-instagram fa-xl"></i></a>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -88,16 +88,7 @@ $isMedewerker = isset($_SESSION['loggedinMedewerker']) && $_SESSION['loggedinMed
             </div>
         </div>
     </div>
-    <footer class="footer items-center p-4 bg-neutral text-neutral-content">
-        <div class="items-center grid-flow-col">
-            <p>@ 2023 Flower Power - All right reserved</p>
-        </div>
-        <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-            <a><i class="fa-brands fa-tiktok fa-xl"></i></a>
-            <a><i class="fa-brands fa-youtube fa-xl"></i></a>
-            <a><i class="fa-brands fa-instagram fa-xl"></i></a>
-        </div>
-    </footer>
+    <?php include './components/footer.php'; ?>
     <script src="./js/scripts.js"></script>
 </body>
 
