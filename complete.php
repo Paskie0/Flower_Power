aa829750201dcc3925aa8bb9f0bccb648c80e22e
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 04:18:26 +0200

    iobhtyy7856875

--- complete.php
+++ complete.php
@@ -15,8 +15,26 @@ include_once './functions/initialize.php';
 
 <body>
     <?php include './components/header.php'; ?>
-    <h1 class="text-7xl">Bedankt voor je bestelling!</h1>
+    <h1 class="pt-4 text-4xl font-bold text-center">Bedankt voor je bestelling!</h1>
+    <div class="divider"></div>
     <?php include './components/footer.php'; ?>
+
+    <script>
+        document.getElementById('createPdfButton').addEventListener('click', function() {
+            // Make a request to the server to generate the PDF
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', '/Flower-Power/functions/generate-pdf.php', true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                    // PDF generation completed, do something with the generated PDF
+                    var pdfUrl = xhr.responseText;
+                    // For example, open the PDF in a new tab
+                    window.open(pdfUrl, '_blank');
+                }
+            };
+            xhr.send();
+        });
+    </script>
 </body>
 
 </html>
