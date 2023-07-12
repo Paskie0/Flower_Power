e2babde37113db4cabc95c231b8bc20e821c1df5
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 05:16:05 +0200

    builjkbkb

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,25 @@ include_once './functions/initialize.php';
     <?php include './components/header.php'; ?>
     <h1 class="pt-4 text-4xl font-bold text-center">Admin Dashboard</h1>
     <div class="divider"></div>
-    <div class=" gap-4 flex">
+    <div class=" gap-4 flex justify-between">
+        <div class="card w-96 bg-base-200 shadow-xl">
+            <div class="card-body">
+                <h2 class="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+        <div class="card w-96 bg-base-200 shadow-xl">
+            <div class="card-body">
+                <h2 class="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
         <div class="card w-96 bg-base-200 shadow-xl">
             <div class="card-body">
                 <h2 class="card-title">Card title!</h2>
