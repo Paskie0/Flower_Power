d3ba4472f17def88614f93298d2045380e9b99a6
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 02:54:11 +0200

    ;lk;kk90999-

--- cart.php
+++ cart.php
@@ -1,6 +1,23 @@
 <?php
 include_once './functions/initialize.php';
 include_once './functions/get-cart-items.php';
+
+// Check if the form for deleting an item is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteItemId'])) {
+    $itemId = $_POST['deleteItemId'];
+
+    // Perform the deletion operation
+    $deleteQuery = "DELETE FROM winkelwagen WHERE klant_id = '$userId' AND artikel_id = '$itemId'";
+
+    if (mysqli_query($conn, $deleteQuery)) {
+        // Refresh the page to update the cart items
+        header('Location: cart.php');
+        exit();
+    } else {
+        // Handle the deletion error
+        $deletionError = true;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,43 +37,19 @@ include_once './functions/get-cart-items.php';
     <ul class="p-4">
         <?php foreach ($cartItems as $item) : ?>
             <li>
-                <?php echo $item['artikel_naam']; ?> (<?php echo $item['hoeveelheid']; ?>) - $<?php echo $item['artikel_prijs']; ?>
-                <button class="delete-button" data-product-id="<?php echo $item['artikel_id']; ?>">Delete</button>
+                <?php echo $item['artikel_naam']; ?> - $<?php echo $item['artikel_prijs']; ?>
+                <form method="post" action="cart.php">
+                    <input type="hidden" name="deleteItemId" value="<?php echo $item['artikel_id']; ?>">
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
             </li>
         <?php endforeach; ?>
     </ul>
+    <?php if (isset($deletionError)) : ?>
+        <p>Error occurred during item deletion. Please try again.</p>
+    <?php endif; ?>
     <a href="checkout.php" class="btn btn-primary m-4">Bestellen</a>
     <?php include './components/footer.php'; ?>
-    <script>
-        // Function to handle the delete button click
-        function deleteCartItem(productId) {
-            // Make an AJAX request to delete the item from the cart
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', './functions/delete-cart-item.php', true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        // Item deleted successfully, reload the cart page
-                        location.reload();
-                    } else {
-                        // Error occurred, display an alert or handle it accordingly
-                        alert('Error deleting item from cart. Please try again.');
-                    }
-                }
-            };
-            xhr.send('productId=' + encodeURIComponent(productId));
-        }
-
-        // Attach event listeners to the delete buttons
-        var deleteButtons = document.getElementsByClassName('delete-button');
-        Array.prototype.forEach.call(deleteButtons, function(button) {
-            button.addEventListener('click', function() {
-                var productId = this.getAttribute('data-product-id');
-                deleteCartItem(productId);
-            });
-        });
-    </script>
 </body>
 
 </html>
