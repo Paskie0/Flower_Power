c78263e48afbf6e7caa247e95201ac9def7fc74b
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 03:16:59 +0200

    piou8y865

--- cart.php
+++ cart.php
@@ -1,6 +1,21 @@
 <?php
 include_once './functions/initialize.php';
 include_once './functions/get-cart-items.php';
+
+if (isset($_POST['deleteItemButton'])) {
+    $deleteItemId = $_POST['deleteItemId'];
+
+    // Query to delete the item from the winkelwagen table
+    $query = "DELETE FROM winkelwagen WHERE artikel_id = '$deleteItemId' AND klant_id = '$userId'";
+
+    // Execute the query
+    if (mysqli_query($conn, $query)) {
+        // Deletion successful
+        // Redirect or display a success message
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($conn);
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +34,13 @@ include_once './functions/get-cart-items.php';
     <h2 class="font-bold p-4">Winkelwagen:</h2>
     <ul class="p-4">
         <?php foreach ($cartItems as $item) : ?>
-            <li><?php echo $item['artikel_naam']; ?> (<?php echo $item['hoeveelheid']; ?>) - $<?php echo $item['artikel_prijs']; ?></li>
+            <li>
+                <?php echo $item['artikel_naam']; ?> (<?php echo $item['hoeveelheid']; ?>) - $<?php echo $item['artikel_prijs']; ?>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                    <input type="hidden" name="deleteItemId" value="<?php echo $item['artikel_id']; ?>">
+                    <button type="submit" class="btn btn-primary" name="deleteItemButton">Delete</button>
+                </form>
+            </li>
         <?php endforeach; ?>
     </ul>
     <a href="checkout.php" class="btn btn-primary m-4">Bestellen</a>
