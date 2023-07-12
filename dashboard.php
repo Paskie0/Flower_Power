ec9defdbbf4ded6f657873e904ef3a7e69968bec
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 05:03:21 +0200

    u987895t7

---
+++ dashboard.php
@@ -0,0 +1,39 @@
+<?php
+include_once './functions/initialize.php';
+include_once './functions/checkout-logic.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flower Power</title>
+    <link rel="stylesheet" href="css/output.css">
+    <script src="https://kit.fontawesome.com/d437031e9c.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php include './components/header.php'; ?>
+    <h1 class="pt-4 text-4xl font-bold text-center">Welcome to Flower Power</h1>
+    <div class="divider"></div>
+    <div class="grid grid-cols-3 gap-4">
+        <a href="#" class="card bg-base-200 shadow-lg rounded-lg overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+            <h2>Flower Arrangements</h2>
+            <p>View our beautiful flower arrangements</p>
+        </a>
+        <a href="#" class="card bg-base-200 shadow-lg rounded-lg overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+            <h2>Plants</h2>
+            <p>Explore our collection of plants</p>
+        </a>
+        <a href="#" class="card bg-base-200 shadow-lg rounded-lg overflow-hidden transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+            <h2>Gifts</h2>
+            <p>Find the perfect gift for any occasion</p>
+        </a>
+    </div>
+    <?php include './components/footer.php'; ?>
+    <script src="https://unpkg.com/daisyui@1.11.1/dist/daisyui.min.js"></script>
+</body>
+
+</html>
