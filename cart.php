271e1883ae10089d5f3c44d1e68e715d41a7b64d
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 03:12:32 +0200

    ojii9uhuf44

--- cart.php
+++ cart.php
@@ -1,35 +1,6 @@
 <?php
-session_start();
-include_once './functions/get-cart-items.php';
 include_once './functions/initialize.php';
-include_once './functions/connect.php';
-
-
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    $userId = $_SESSION['user_id'];
-
-    // Check if the form is submitted and the item ID is provided
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteItemId'])) {
-        $itemId = $_POST['deleteItemId'];
-
-        // Query to delete the item from the cart
-        $deleteQuery = "DELETE FROM winkelwagen WHERE klant_id = '$userId' AND artikel_id = '$itemId'";
-
-        // Execute the query
-        if (mysqli_query($conn, $deleteQuery)) {
-            // Deletion successful, refresh the page
-            header('Location: cart.php');
-            exit();
-        } else {
-            // Error occurred during deletion
-            echo 'Failed to delete the item from the cart.';
-        }
-    }
-} else {
-    // User is not logged in, handle accordingly
-    header('Location: login.php');
-    exit();
-}
+include_once './functions/get-cart-items.php';
 ?>
 
 <!DOCTYPE html>
@@ -48,13 +19,7 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     <h2 class="font-bold p-4">Winkelwagen:</h2>
     <ul class="p-4">
         <?php foreach ($cartItems as $item) : ?>
-            <li>
-                <?php echo $item['artikel_naam']; ?> - $<?php echo $item['artikel_prijs']; ?>
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                    <input type="hidden" name="deleteItemId" value="<?php echo $item['artikel_id']; ?>">
-                    <button type="submit" class="btn btn-primary">Delete</button>
-                </form>
-            </li>
+            <li><?php echo $item['artikel_naam']; ?> (<?php echo $item['hoeveelheid']; ?>) - $<?php echo $item['artikel_prijs']; ?></li>
         <?php endforeach; ?>
     </ul>
     <a href="checkout.php" class="btn btn-primary m-4">Bestellen</a>
