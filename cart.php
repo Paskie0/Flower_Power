aa88936faf9fd354850d45e1d536c1b1b71c7aa4
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 03:46:27 +0200

    niuujvb uhjvc

--- cart.php
+++ cart.php
@@ -4,6 +4,16 @@ include_once './functions/get-cart-items.php';
 
 // Check if the deleteItemId is set
 if (isset($_POST['deleteItemId'])) {
+
+    $servername = "localhost";
+    $username = "u597563256_Pascal";
+    $password = "3Lf6aR3s";
+    $dbname = "u597563256_Flower_Power";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
     // Get the deleteItemId from the POST array
     $deleteItemId = $_POST['deleteItemId'];
 
