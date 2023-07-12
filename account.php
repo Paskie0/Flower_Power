d044a39fc0a0a7db60f83ad5e903f9fba2627869
Pascal De Wit <Pascaldewit@outlook.com>
2023-07-12 05:59:16 +0200

    lhjkv

--- account.php
+++ account.php
@@ -77,7 +77,7 @@ include_once './functions/account-info.php';
                 <div class="flex items-center justify-between">
                     <div class="flex-grow">
                         <h2 class="hidden">klant_email</h2>
-                        <h2 class="text-lg font-semibold">Email</h2>
+                        <h3 class="text-lg font-semibold">Email</h3>
                         <p class="text-gray-500">' . $email . '</p>
                     </div>
                     <button class="px-4 py-2 bg-blue-500 text-white rounded-lg edit-btn">Edit</button>
